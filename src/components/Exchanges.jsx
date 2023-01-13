615d8d194dc197d226d5f89d66c0f69d5eb2be98
sfk-aayan <190041116faiyaz@gmail.com>
2023-01-13 08:47:43 +0600

    Final Changes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -236,7 +236,7 @@ const Exchanges = ({ email }) => {
             {isShown && (
               <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
               
-                  <select className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism" 
+                  <select className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-black border-none text-sm white-glassmorphism" 
                           onChange={(event) => {setCoin(event.target.value); }} name="coins" id="coins" placeholder="Select a Crypto"
                           optionFilterProp="children"
                           filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
@@ -258,7 +258,7 @@ const Exchanges = ({ email }) => {
                       onClick={setbuyingPrice}
                       className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
                     >
-                      Send now
+                      Buy
                     </button>
     
               </div>
